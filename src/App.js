3b5656c6bcb7aa3f67b7632df31057db97d5573d
Bazuuko <140450044+Bazuuko@users.noreply.github.com>
2023-08-16 23:27:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -828,102 +828,12 @@ The Shibarium Punks is a NFT collection of 24x24 pixelated artworks generated al
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/2.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/3.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/4.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/5.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/6.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/7.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
             src={"/config/punks/8.png"}
           />
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/9.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/10.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/11.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/12.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/13.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/14.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/15.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/16.png"}
-          />
-          </div>
-<div class="slide">
-          <StyledImg3
-            src={"/config/punks/49.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/50.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/51.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/56.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
-            src={"/config/punks/34.png"}
+            src={"/config/punks/46.png"}
           />
           </div>
           <div class="slide">
@@ -933,7 +843,12 @@ The Shibarium Punks is a NFT collection of 24x24 pixelated artworks generated al
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/53.png"}
+            src={"/config/punks/52.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/95.png"}
           />
           </div>
           <div class="slide">
@@ -948,32 +863,47 @@ The Shibarium Punks is a NFT collection of 24x24 pixelated artworks generated al
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/21.png"}
-          />
-          </div>
-<div class="slide">
-          <StyledImg3
-            src={"/config/punks/35.png"}
-          />
-          </div>
-          <div class="slide">
-          <StyledImg3
             src={"/config/punks/14.png"}
           />
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/55.png"}
+            src={"/config/punks/12.png"}
           />
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/57.png"}
+            src={"/config/punks/10.png"}
           />
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/95.png"}
+            src={"/config/punks/8.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/6.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/4.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/2.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/1.png"}
+          />
+          </div>
+<div class="slide">
+          <StyledImg3
+            src={"/config/punks/11.png"}
           />
           </div>
           <div class="slide">
@@ -983,7 +913,77 @@ The Shibarium Punks is a NFT collection of 24x24 pixelated artworks generated al
           </div>
           <div class="slide">
           <StyledImg3
-            src={"/config/punks/43.png"}
+            src={"/config/punks/58.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/40.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/35.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/51.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/50.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/38.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/9.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/3.png"}
+          />
+          </div>
+<div class="slide">
+          <StyledImg3
+            src={"/config/punks/5.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/7.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/11.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/47.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/33.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/97.png"}
+          />
+          </div>
+          <div class="slide">
+          <StyledImg3
+            src={"/config/punks/24.png"}
           />
           </div>
 
